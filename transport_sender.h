d84045a31460f5a48a2e822ebf47e3c61e88c2bb
ztblick <zach.blickensderfer@gmail.com>
2026-02-05 15:01:36 -0800

    Continuing to develop interfaces for sender and receiver.

--- transport_sender.h
+++ transport_sender.h
@@ -1,3 +1,40 @@
 #pragma once
 
-void create_sender(void);
+/**
+ * Key design choice:
+ *  - Application layer calls to send_transmission() will NOT return
+ *    until all data in the transmission has been ACK'd by the receiver.
+ *    (At least, for now -- v1).
+ *
+ * Initializes data structures and launches threads for the sender:
+ *  - TKTKTK
+ */
+VOID create_sender(VOID);
+
+/**
+ * This is called from send_transmission, who will specify the
+ * offset into the transmission as well as the amount of data to
+ * split into packets and send to the network layer.
+ *
+ * This function creates the individual packets. For security purposes,
+ * the memcpy into the packet is protected with a try-except.
+ *
+ * @param transmission_data The offset into the transmission where
+ * we begin packetizing.
+ * @param bytes_to_packetize The number of bytes to packetize.
+ */
+VOID packetize_contiguous(PVOID transmission_data, ULONG64 bytes_to_packetize);
+
+
+/**
+ * Clears the packet buffer, sending all packets to the network.
+ *  If the network rejects a packet, this function will again attempt
+ *  to send the packet.
+ *
+ *  This function needs to listen for the network shutdown event, otherwise
+ *  it will never end.
+ */
+VOID send_packet_batch(ULONG64 number_of_packets_to_send);
+
+
+DWORD sender_listener(LPVOID param);
